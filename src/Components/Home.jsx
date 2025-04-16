c954cb77222645489078eb2b38b2dff6ad57686e
Mayur-Murarka <137669111+Mayur-Murarka@users.noreply.github.com>
2025-04-16 19:44:18 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
     </p>
 
     <div>
-    <Link to="project" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+    <Link to="projects" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
     Projects
     <span className="group-hover:rotate-90 duration-300">
     <MdOutlineKeyboardArrowRight size={25}
